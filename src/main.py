236cc982393e373d36515a7d182a28af610e0ef0
Jaehoon Song <jhssong02@gmail.com>
2025-04-12 00:42:40 +0900

    feat: Add global exception handler (#5)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from exceptions.handler import register_exception_handlers
+# TODO Remove TestExeption
+from exceptions.definitions import TestException
 
 from src.auth.router import router as auth_router
 
@@ -8,8 +11,16 @@ app = FastAPI(
     description="",
     version="1.0.0"
     )
+
+register_exception_handlers(app)
+
 app.include_router(auth_router)
 
 @app.get("/", summary="Test API")
 def read_root():
     return {"message": "Hello World"}
+
+# TODO Remove TestExeption
+@app.get("/oh-no")
+def app_exception_handler():
+    raise TestException("Oh no")
